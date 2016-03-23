1c44dc82e4ba98681a82103a524ac63f2385320b
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-23 09:57:29 -0500

    cleaning up smoe devise messes. working from master because I forgot an I am a monster

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,9 +2,11 @@ class MainController < ApplicationController
   
   def home
     render "home"
+  end
 
   def about
     render "about"
+  end
 
   def discover
     render "discover"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'main/about' => "main#about"
-  get 'main/sign_in' => "main#sign_in"
-  get 'main/sign_up' => "main#sign_up"
-  get 'main/home' => "main#home"
-  get 'main/discover' => "main#discover"
+  get '/about' => "main#about"
+  get '/sign_in' => "main#sign_in"
+  get '/sign_up' => "main#sign_up"
+  get '/' => "main#home"
+  get '/discover' => "main#discover"
   
   get 'tutorials/create' => "tutorials#create"
   post 'tutorials/create/confirmation'=> "tutorials#create_confirmation"
