bc2baacfba1c8fa9b1e9cfc9206531ff7cd696a7
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-23 16:43:20 -0500

    can successfully add company to db from create & confirmation page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -11,7 +11,7 @@ class CompaniesController < ApplicationController
   end
 
   def edit
-    @new_company = Company.new
+    @new_company = Company.new(:profile_image => params[:companies][:profile_image], :name => params[:companies][:name], :bio => params[:companies][:bio], :address_1 => params[:companies][:address_1], :address_2 => params[:companies][:address_2], :city => params[:companies][:city], :state => params[:companies][:state], :zip => params[:companies][:zip])
     render "edit"
   end
 
