d39f1a2854288c46f61a786d5fb522cad2ba0e15
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-24 11:16:38 -0500

    fixed error. was missing a params identifier

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -10,8 +10,8 @@ class StepsController < ApplicationController
   def create_confirmation
     @tutorial = Tutorial.find_by_id(params[:tutorial_id])
     @chapter = Chapter.find_by_id(params[:chapter_id])
-    @new_step = Step.new(:title => params[:step][:title], :description => params[:step][:description], :draft => params[:step][:draft], :warning =>[:step][:warning], :start_time => (params[:step][:start_time]).to_i, :end_time => (params[:step][:end_time]).to_i :chapter_id => @chapter.id)
+    @new_step = Step.new(:title => params[:step][:title], :description => params[:step][:description], :draft => params[:step][:draft], :warning =>params[:step][:warning], :start_time => (params[:step][:start_time]).to_i, :end_time => (params[:step][:end_time]).to_i, :chapter_id => @chapter.id)
     @new_step.save
-    render "/"
+    redirect_to "/"
   end
 end
