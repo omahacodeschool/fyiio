28d5482c636915b088de7cb5e16acf50d4cb9c3e
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-28 18:49:36 -0500

    reverting back to earlier version

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,18 +1,6 @@
 class SearchesController < ApplicationController
-  def title_matches
-    @title_matches = @tutorials.search_by_title(params[:q])
-  end
-
-  def description_matches
-    @description_matches = @tutorials.search_by_description(params[:q])
-  end
-
   def index
     @tutorials = Tutorial.where(public: true, draft: false)
-    if title_matches.empty? == false
-      @results = @tutorials.search_by_title(params[:q])
-    else description_matches.empty? == false
-      @results = @tutorials.search_by_description(params[:q])
-    end
+    @results = @tutorials.search_by_title(params[:q])
   end
 end
