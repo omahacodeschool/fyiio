8ebe2a18c1b2372f4d0b1a599b799d38ed8b8f16
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-28 18:27:24 -0500

    can't remember how to call this, have done it before

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,19 +1,18 @@
 class SearchesController < ApplicationController
   def title_matches
-    @tutorials = Tutorial.where(public: true, draft: false)
     @title_matches = @tutorials.search_by_title(params[:q])
   end
 
   def description_matches
-    @tutorials = Tutorial.where(public: true, draft: false)
-    @description_matches = @tutorials.search_by_description(params[:q])    
+    @description_matches = @tutorials.search_by_description(params[:q])
   end
 
   def index
-    if title_matches.length > 0 || description_matches.length > 0
-      @tutorials = Tutorial.where(public: true, draft: false)
+    @tutorials = Tutorial.where(public: true, draft: false)
+    if title_matches.empty? == false
       @results = @tutorials.search_by_title(params[:q])
-    else
+    else description_matches.empty? == false
+      @results = @tutorials.search_by_description(params[:q])
     end
   end
 end
