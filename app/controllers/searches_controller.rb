e40a06e23f5241a6f7a7572eb4ef6386a858a6dc
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-28 16:42:45 -0500

    code to search tutorial descriptions does not break the site, but I need to add to the tutorial database to test if the code works

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
   def index
-    @results = Tutorial.where(:public => true, :draft => false).search_by_title(params[:q])
+    @results = Tutorial.where(:public => true, :draft => false).search_by_title(params[:q]) || Tutorial.where(:public => true, :draft => false).search_by_description
   end
 end
