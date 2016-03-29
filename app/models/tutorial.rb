bd745436d4c63c2f747501ce681051e1d06de403
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-28 19:23:42 -0500

    trying to update pg search in tutorials model

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,6 +2,7 @@ class Tutorial < ActiveRecord::Base
   has_many :chapters, dependent: :destroy
   include PgSearch
   pg_search_scope :search_by_title, :against => :title
+  pg_search_scope :search_by_description, :against => :description
   
   mount_uploader :video, VideoUploader
 end
