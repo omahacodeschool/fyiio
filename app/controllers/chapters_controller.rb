765d91d0329ff611da81631f6a70d83160c25599
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-29 01:42:22 -0500

    for validation for chapters working--set on controllers, responds in edit and create view page

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -11,16 +11,20 @@ class ChaptersController < ApplicationController
     @current_user = User.find_by_id(current_user)
     @tutorial = Tutorial.find_by_id(params[:tutorial_id])
     @new_chapter = Chapter.new(title: params[:chapter][:title], description: params[:chapter][:description], draft: params[:chapter][:draft], start_time: (params[:chapter][:start_time]).to_i, end_time: (params[:chapter][:end_time]).to_i, tutorial_id: @tutorial.id)
-    @new_chapter.save
-    flash[:success] = "Chapter Created!"
-    redirect_to chapters_edit_path(@tutorial.id, @new_chapter.id)
+    if @new_chapter.valid?
+      @new_chapter.save
+      flash[:success] = "Chapter Created!"
+      redirect_to chapters_edit_path(@tutorial.id, @new_chapter.id)
+    else
+      render "create"
+    end
   end
 
   def edit
     @current_user = User.find_by_id(current_user)
     @tutorial = Tutorial.find_by_id(params[:tutorial_id])
     @edit_chapter = Chapter.find_by_id(params[:chapter_id])
-    @steps = Step.where({chapter_id: @edit_chapter.id}).order('id')
+    @steps = @edit_chapter.steps.order('id')
     render "edit"
   end
 
@@ -28,10 +32,15 @@ class ChaptersController < ApplicationController
     @current_user = User.find_by_id(current_user)
     @tutorial = Tutorial.find_by_id(params[:tutorial_id])
     @edit_chapter = Chapter.find_by_id(params[:chapter_id])
+    @steps = @edit_chapter.steps.order('id')
     @edit_chapter.update(title: params[:chapter][:title], description: params[:chapter][:description], draft: params[:chapter][:draft], start_time: params[:chapter][:start_time], end_time: params[:chapter][:end_time])
-    flash[:success] = "Chapter Successfully Updated!"
-    @edit_chapter.save
-    redirect_to chapters_edit_path(@tutorial.id, @edit_chapter.id)
+    if @edit_chapter.valid?
+      flash[:success] = "Chapter Successfully Updated!"
+      @edit_chapter.save
+      redirect_to chapters_edit_path(@tutorial.id, @edit_chapter.id)
+    else
+      render "edit"
+    end
   end
 
   def delete
--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,5 +1,9 @@
 class Chapter < ActiveRecord::Base
   has_many :steps, dependent: :destroy
   belongs_to(:tutorial)
-  validates :description, :length => { :maximum => 250 }
+  validates :description, :length => { 
+    :maximum => 250,
+    :too_long  => "Description cannot exceed 250 characters" 
+  }
+  validates_presence_of :title, :tutorial_id, :start_time, :end_time
 end
