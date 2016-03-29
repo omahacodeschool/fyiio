6fc5e856d27fcefb93046532f1dfbff2e997848a
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-29 09:00:35 -0500

    resulted in new error NoMethodError in Searches#index

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 class SearchesController < ApplicationController
   def index
     @tutorials = Tutorial.where(public: true, draft: false)
-    @results = @tutorials.search_by_title(params[:q])
+    # @results = @tutorials.search_by_title(params[:q])
+    @results = @tutorials.multisearchable(params[:q])
   end
 end
