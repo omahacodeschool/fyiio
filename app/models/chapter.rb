66c28e89d7668e42955bef35101e315486c02399
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-29 13:05:32 -0500

    time conversion method for chapters added to view page

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -6,4 +6,11 @@ class Chapter < ActiveRecord::Base
     :too_long  => "Description cannot exceed 250 characters" 
   }
   validates_presence_of :title, :tutorial_id, :start_time, :end_time
+
+  def video_time_conversion_for_chapters
+    start_time = Time.at(self.start_time).utc.strftime("%H:%M:%S")
+    end_time = Time.at(self.end_time).utc.strftime("%H:%M:%S")
+
+    return "#{start_time} - #{end_time}"
+  end
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -5,4 +5,11 @@ class Step < ActiveRecord::Base
     :too_long  => "Description cannot exceed 250 characters" 
   }
   validates_presence_of :title, :chapter_id, :start_time, :end_time
+
+  def video_time_conversion_for_steps
+    start_time = Time.at(self.start_time).utc.strftime("%H:%M:%S")
+    end_time = Time.at(self.end_time).utc.strftime("%H:%M:%S")
+
+    return "#{start_time} - #{end_time}"
+  end
 end
