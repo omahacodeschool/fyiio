8f6b7bacb8ddbe4e319f976d3c14909c47601d57
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-29 08:52:22 -0500

    tias some pgsearch

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,6 @@
 class SearchesController < ApplicationController
   def index
     @tutorials = Tutorial.where(public: true, draft: false)
-    # @results = @tutorials.search_by_title(params[:q])
-    @results = @tutorials.where("title LIKE ? or description LIKE ?", "%#{:q}%", "%#{:q}%")
+    @results = @tutorials.search_by_title(params[:q])
   end
 end
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,8 +1,8 @@
 class Tutorial < ActiveRecord::Base
   has_many :chapters, dependent: :destroy
   include PgSearch
-  pg_search_scope :search_by_title, :against => :title
-  pg_search_scope :search_by_description, :against => :description
+  # pg_search_scope :search_by_title, :against => :title
+  multisearchable :against => [:title, :description]
   
   mount_uploader :video, VideoUploader
 end
