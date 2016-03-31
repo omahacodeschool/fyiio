78df8f7bf88a63081d104ca305771c50ca63fec1
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-31 09:03:01 -0500

    js is a demon and not working anymore.

--- app/assets/javascripts/video_player.js
+++ app/assets/javascripts/video_player.js
@@ -1,14 +1,38 @@
-var myPlayer = videojs('my-video');
-var startTime = document.getElementById("chapter_start_time");
-var endTime = document.getElementById("chapter_end_time");
+window.onload = function() {
+  var chapterPlayer = videojs('chapter-video');
+  var stepPlayer = videojs('step-video');
+  var chapterStartTime = document.getElementById("chapter_start_time");
+  var chapterEndTime = document.getElementById("chapter_end_time");
+  var stepStartTime = document.getElementById("step_start_time");
+  var stepEndTime = document.getElementById("step_end_time");
+  var chapterStartButton = document.getElementById("setChapterStartTime");
+  var chapterEndButton = document.getElementById("setChapterEndTime");
+  var stepStartButton = document.getElementById("setStepStartTime");
+  var stepEndButton = document.getElementById("setStepEndTime");
 
-document.getElementById("setStartTime").addEventListener("click", function(){
-  startTime.value = myPlayer.currentTime();
-  myPlayer.pause()
+  function captureVideoStartTime(start, player){
+    start.value = player.currentTime();
+    player.pause();
+  };
 
-});
+  function captureVideoEndTime(end, player){
+    end.value = player.currentTime();
+    player.pause();
+  };
 
-document.getElementById("setEndTime").addEventListener("click", function(){
-  endTime.value = myPlayer.currentTime();
-  myPlayer.pause()
-});
+  chapterStartButton.addEventListener("click", function(){
+    var startOutcome = captureVideoStartTime(chapterStartTime, chapterPlayer);
+  });
+
+  chapterEndButton.addEventListener("click", function(){
+    var endOutcome = captureVideoEndTime(chapterEndTime, chapterPlayer);
+  });
+
+  stepStartButton.addEventListener("click", function(){
+    var startOutcome = captureVideoStartTime(stepStartTime, stepPlayer);
+  });
+
+  stepEndButton.addEventListener("click", function(){
+    var endOutcome = captureVideoEndTime(stepEndTime, stepPlayer);
+  });
+};
