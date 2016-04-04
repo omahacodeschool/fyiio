b9958a65ebd4e54c79e9107d1c76cd8a1a76f380
Amy Wall <amymariewall@gmail.com>
2016-04-04 18:55:17 -0500

    Javascript file in progress.

--- app/assets/javascripts/chapters.js
+++ app/assets/javascripts/chapters.js
@@ -17,25 +17,46 @@ function addStepField() {
           "chapter_steps_attributes_0_draft".replace("0", mSec);
     nameAttributDraft =  
           "chapter[steps_attributes][0][draft]".replace("0", mSec);
+
+    idAttributWarning =  
+          "chapter_steps_attributes_0_warning".replace("0", mSec);
+    nameAttributWarning =  
+          "chapter[steps_attributes][0][warning]".replace("0", mSec);
+
+    idAttributDescription =  
+          "chapter_steps_attributes_0_description".replace("0", mSec);
+    nameAttributDescription =  
+          "chapter[steps_attributes][0][description]".replace("0", mSec);
+ 
+    idAttributStartTime =  
+          "chapter_steps_attributes_0_start_time".replace("0", mSec);
+    nameAttributStartTime =  
+          "chapter[steps_attributes][0][start_time]".replace("0", mSec);
+
+    idAttributEndTime =  
+          "chapter_steps_attributes_0_end_time".replace("0", mSec);
+    nameAttributEndTime =  
+          "chapter[steps_attributes][0][end_time]".replace("0", mSec);
+  
         
     //create <li> tag
     var li = document.createElement("li");
  
-    //create label for Kind, set it's for attribute, 
+    //create label for Title, set it's for attribute, 
     //and append it to <li> element
-    var labelKind = document.createElement("label");
-    labelKind.setAttribute("for", idAttributKind);
-    var kindLabelText = document.createTextNode("Kind");
-    labelKind.appendChild(kindLabelText);
-    li.appendChild(labelKind);
+    var labelTitle = document.createElement("label");
+    labelTitle.setAttribute("for", idAttributTitle);
+    var titleLabelText = document.createTextNode("Title");
+    labelTitle.appendChild(titleLabelText);
+    li.appendChild(labelTitle);
  
-    //create input for Kind, set it's type, id and name attribute, 
+    //create input for Title, set it's type, id and name attribute, 
     //and append it to <li> element
-    var inputKind = document.createElement("INPUT");
-    inputKind.setAttribute("type", "text");
-    inputKind.setAttribute("id", idAttributKind);
-    inputKind.setAttribute("name", nameAttributKind);
-    li.appendChild(inputKind);
+    var inputTitle = document.createElement("INPUT");
+    inputTitle.setAttribute("type", "text");
+    inputTitle.setAttribute("id", idAttributTitle);
+    inputTitle.setAttribute("name", nameAttributTitle);
+    li.appendChild(inputTitle);
  
     //create label for Street, set it's for attribute, 
     //and append it to <li> element
