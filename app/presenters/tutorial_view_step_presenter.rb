ac9b857473134a76255b0c33181976bcf91d3e46
Amy Wall <amymariewall@gmail.com>
2016-04-04 15:22:07 -0500

    Added a seed file. Added a method in the step_presenter class to show the amount of seconds left in chapter's video after step is complete.

--- app/presenters/tutorial_view_step_presenter.rb
+++ app/presenters/tutorial_view_step_presenter.rb
@@ -48,5 +48,21 @@ class StepPresenter
       "#{@step_order} #{@step.title} - (#{@step.video_time_conversion_for_steps})"
   end
 
+  # TODO Figure out how to prevent chapter length from being shorter than the sum of alls steps. 
+  # This is currently returning a number of seconds left in video after that step is complete. TODO use that data (or modify it) to display a progress bar for user. Consider using this data to update a progress bar when a user clicks to go to a next step and/or hits a timestamp on a video.
+
+  def get_percentage_left_in_chapter
+    chapter_length = (@chapter.end_time - @chapter.start_time)
+    time_left = (chapter_length - @step.end_time)
+      #if chapter_length != 0.0
+       # percentage_left = (time_left/chapter_length)
+      #else
+       # percentage_left = 0
+      #end
+    return time_left
+  end
+
+
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+companies = Company.create([
+  {name: 'Pixar', username: 'pixar', address_1: '1234 Sesame Street', city: 'Omaha', state: 'Nebraska', zip: '68106'},
+  {name: 'Disney', username: 'disney', address_1: '1235 Yellow Brick Road', city: 'Omaha', state: 'Nebraska', zip: '68106'}])
+
+
+tutorials = Tutorial.create([
+  {title: 'Intro to ActiveRecord', description: 'This is an intro to ActiveRecord', video: 'https://youtu.be/epMyXW7TjwE', public: false, user_id: 1, category: 'ActiveRecord', draft: false},
+  {title: 'Intro to ActiveRecord II', description: 'This is the other intro to ActiveRecord', video: 'https://youtu.be/ghfirzoQZ3Q', public: true, user_id: 1, category: 'ActiveRecord', draft: false},
+  {title: 'Post Requests', description: 'How to use post requests', video: 'https://youtu.be/QHqMMiBA7ew', public: false, user_id: 2, category: 'HTTP', draft: false},
+  {title: 'Dropdowns', description: 'How to create HTML dropdowns', video: 'https://youtu.be/-SszsSObbg0', public: true, user_id: 2, category: 'HTML', draft: true}])
+
+users = User.create([
+  {email: 'tommy@aol.com', password: 'hello314', first_name: 'Tommy', last_name: 'Tutone', company_id: 1},
+  {email: 'pete@peterson.com', password: 'Password123', first_name: 'Pete', last_name: 'Peterson', company_id: 2}])
+
+
+# votes = Vote.create([
+#   {user_id: 1},
+#   {user_id: 1},
+#   {user_id: 2},
+#   {user_id: 2}])
