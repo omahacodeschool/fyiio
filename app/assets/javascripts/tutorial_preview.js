f2ff60569b34036b5a7e744a162b04f0af478235
Ryan Newell <youcatastrophe@gmail.com>
2016-04-05 13:01:49 -0500

    WIP. Starting with most basic idea. Just display the information first, without it being hidden and without having to click 'preview' first.

---
+++ app/assets/javascripts/tutorial_preview.js
@@ -0,0 +1,8 @@
+// Once user has completed a search and its search results have 
+// been displayed on the page, the user should be able to click 
+// on the word 'preview' and get a preview in the
+// form of an overlay window that shows info such as:
+// description, number of  chapters, estimated completion time.
+
+// What triggers the preview info? Clicking on 'preview'
+// 
