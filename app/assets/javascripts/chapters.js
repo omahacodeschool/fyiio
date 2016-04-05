c99f0bc7cb56e3d127a72582253b046481b28c3b
Amy Wall <amymariewall@gmail.com>
2016-04-05 14:10:00 -0500

    All fields added to js function to add more steps.

--- app/assets/javascripts/chapters.js
+++ app/assets/javascripts/chapters.js
@@ -39,6 +39,9 @@ function addStepField() {
           "chapter[steps_attributes][0][end_time]".replace("0", mSec);
   
         
+    var ul = document.getElementById("myList");
+
+
     //create <li> tag
     var li = document.createElement("li");
  
@@ -57,27 +60,124 @@ function addStepField() {
     inputTitle.setAttribute("id", idAttributTitle);
     inputTitle.setAttribute("name", nameAttributTitle);
     li.appendChild(inputTitle);
+
+    //append the Title li to the <ul> element
+    ul.appendChild(li);
+
+    //create <li> tag
+    var draftLi = document.createElement("li");
+
  
-    //create label for Street, set it's for attribute, 
+    //create label for Draft, set it's for attribute, 
     //and append it to <li> element
-    var labelStreet = document.createElement("label");
-    labelStreet.setAttribute("for", idAttributStreet);
-    var streetLabelText = document.createTextNode("Street");
-    labelStreet.appendChild(streetLabelText);
-    li.appendChild(labelStreet);
+    var labelDraft = document.createElement("label");
+    labelDraft.setAttribute("for", idAttributDraft);
+    var draftLabelText = document.createTextNode("Draft");
+    labelDraft.appendChild(draftLabelText);
+    draftLi.appendChild(labelDraft);
  
-    //create input for Street, set it's type, id and name attribute, 
+    //create input for Draft, set it's type, id and name attribute, 
     //and append it to <li> element
-    var inputStreet = document.createElement("INPUT");
-    inputStreet.setAttribute("type", "text");
-    inputStreet.setAttribute("id", idAttributStreet);
-    inputStreet.setAttribute("name", nameAttributStreet);
-    li.appendChild(inputStreet);
+    var inputDraft = document.createElement("INPUT");
+    inputDraft.setAttribute("type", "checkbox");
+    inputDraft.setAttribute("id", idAttributDraft);
+    inputDraft.setAttribute("name", nameAttributDraft);
+    draftLi.appendChild(inputDraft);
+
+    //append the Title li to the <ul> element
+    ul.appendChild(draftLi);
+
+    //create <li> tag
+    var warningLi = document.createElement("li");
+
  
-    //add created <li> element with its child elements 
-    //(label and input) to myList (<ul>) element
-    document.getElementById("myList").appendChild(li);
+    //create label for Warning, set it's for attribute, 
+    //and append it to <li> element
+    var labelWarning = document.createElement("label");
+    labelWarning.setAttribute("for", idAttributWarning);
+    var warningLabelText = document.createTextNode("Warning");
+    labelWarning.appendChild(warningLabelText);
+    warningLi.appendChild(labelWarning);
  
-    //show address header
+    //create input for Warning, set it's type, id and name attribute, 
+    //and append it to <li> element
+    var inputWarning = document.createElement("INPUT");
+    inputWarning.setAttribute("type", "checkbox");
+    inputWarning.setAttribute("id", idAttributWarning);
+    inputWarning.setAttribute("name", nameAttributWarning);
+    warningLi.appendChild(inputWarning);
+
+    //append the Title li to the <ul> element
+    ul.appendChild(warningLi);
+
+
+    //create <li> tag
+    var descriptionLi = document.createElement("li");
+ 
+    //create label for Description, set it's for attribute, 
+    //and append it to <li> element
+    var labelDescription = document.createElement("label");
+    labelDescription.setAttribute("for", idAttributDescription);
+    var descriptionLabelText = document.createTextNode("Description");
+    labelDescription.appendChild(descriptionLabelText);
+    descriptionLi.appendChild(labelDescription);
+ 
+    //create input for Description, set it's type, id and name attribute, 
+    //and append it to <li> element
+    var inputDescription = document.createElement("INPUT");
+    inputDescription.setAttribute("type", "textarea");
+    inputDescription.setAttribute("id", idAttributDescription);
+    inputDescription.setAttribute("name", nameAttributDescription);
+    descriptionLi.appendChild(inputDescription);
+
+    //append the description li to the <ul> element
+    ul.appendChild(descriptionLi);
+
+     //create <li> tag
+    var startTimeLi = document.createElement("li");
+ 
+    //create label for StartTime, set it's for attribute, 
+    //and append it to <li> element
+    var labelStartTime = document.createElement("label");
+    labelStartTime.setAttribute("for", idAttributStartTime);
+    var startTimeLabelText = document.createTextNode("Start Time");
+    labelStartTime.appendChild(startTimeLabelText);
+    startTimeLi.appendChild(labelStartTime);
+ 
+    //create input for StartTime, set it's type, id and name attribute, 
+    //and append it to <li> element
+    var inputStartTime = document.createElement("INPUT");
+    inputStartTime.setAttribute("type", "text");
+    inputStartTime.setAttribute("id", idAttributStartTime);
+    inputStartTime.setAttribute("name", nameAttributStartTime);
+    startTimeLi.appendChild(inputStartTime);
+
+    //append the StartTime li to the <ul> element
+    ul.appendChild(startTimeLi);
+
+    //create <li> tag
+    var endTimeLi = document.createElement("li");
+ 
+    //create label for EndTime, set it's for attribute, 
+    //and append it to <li> element
+    var labelEndTime = document.createElement("label");
+    labelEndTime.setAttribute("for", idAttributEndTime);
+    var endTimeLabelText = document.createTextNode("End Time");
+    labelEndTime.appendChild(endTimeLabelText);
+    endTimeLi.appendChild(labelEndTime);
+ 
+    //create input for EndTime, set it's type, id and name attribute, 
+    //and append it to <li> element
+    var inputEndTime = document.createElement("INPUT");
+    inputEndTime.setAttribute("type", "text");
+    inputEndTime.setAttribute("id", idAttributEndTime);
+    inputEndTime.setAttribute("name", nameAttributEndTime);
+    endTimeLi.appendChild(inputEndTime);
+
+    //append the EndTime li to the <ul> element
+    ul.appendChild(endTimeLi);
+
+ 
+    //show step header
     $("#stepHeader").show(); 
 }
