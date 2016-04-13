57741ce633d7ec462d0e78c0c40eab448bdf9e57
Amy Wall <amymariewall@gmail.com>
2016-04-13 16:35:43 -0500

    Chapter displays now set to initially be hidden.

--- app/assets/javascripts/tutorials.js
+++ app/assets/javascripts/tutorials.js
@@ -7,6 +7,7 @@ $("#showNewChapterForm").on('click', function(event){
       $("#showNewChapterForm").hide();
     });
 
+$(".chapterDisplay").hide();
 
 $("#tutorialOverview").on('click', function() {
   $("#chapterTitlesAndDescriptions").show();
@@ -21,42 +22,5 @@ $(".showChapterTab").on('click', function(event) {
   $("#chapterTitlesAndDescriptions").hide();
   $(".chapterDisplay").hide();
   $("#chapterContentDisplay" + currentChapter).show();
-})
+});
 
-
-//   $('#tutorial').show();
-//   // $('#chapterTitlesAndDescriptions').show();
-//   $('#chapterDisplay').hide();
-
-//   $('.showChapterTab').on('click', function (event) {
-//     var chapterTab = event.target;
-//     var chapterId = chapterTab.parentElement.value;
-//     // var chapterDisplay = document.getElementById("chapter"+chapterId)
-//     // var chapterDisplayDiv = document.getElementById(chapterId);
-//     $('#chapterTitlesAndDescriptions').hide();
-//     $('.chapterContentDisplay').hide();
-//     $('#chapterContentDisplay'+chapterId).show();
-//     // chapterDisplay.style.display = "";
-//     // $('#chapter'+$(chapterId).val()).show();
-//   // $('.showChapterTab').on('click', function(event) {
-//   //   var tab = event.target;
-//   //   var chapterNumber = tab.value;
-//   //   $('#'+chapterNumber).show();
-//   })
-
-//   // $('#option1').show();
-//   // $('#selectMe').change(function () {
-//   //   $('.group').hide();
-//   //   $('#'+$(this).val()).show();
-//   });
-// // });
-
-
-// $(document).ready(function () {
-//   $('.group').hide();
-//   $('#option1').show();
-//   $('#selectMe').change(function () {
-//     $('.group').hide();
-//     $('#'+$(this).val()).show();
-//   })
-// });
