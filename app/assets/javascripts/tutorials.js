f9e31692a4e548a96199956ffb52bee0f18b4a46
Amy Wall <amymariewall@gmail.com>
2016-04-13 14:43:30 -0500

    JavaScript to show the correct chapter when you click on the appropriate div is almost working.

--- app/assets/javascripts/tutorials.js
+++ app/assets/javascripts/tutorials.js
@@ -6,3 +6,36 @@ $("#showNewChapterForm").on('click', function(event){
       chapterForm.style.display = "";
       $("#showNewChapterForm").hide();
     });
+
+$(document).ready(function () {
+  $('.chapterContentDisplay').hide();
+  $('#chapterTitlesAndDescriptions').show();
+  $('.showChapterTab').on('click', function (event) {
+    var tabLink = event.target;
+    var chapterId = tabLink.parent.value;
+    var chapterDisplayDiv = document.getElementById(chapterId);
+    $('#chapterTitlesAndDescriptions').hide();
+    $('.chapterContentDisplay').hide();
+    chapterDisplayDiv.show();
+  // $('.showChapterTab').on('click', function(event) {
+  //   var tab = event.target;
+  //   var chapterNumber = tab.value;
+  //   $('#'+chapterNumber).show();
+  })
+
+  // $('#option1').show();
+  // $('#selectMe').change(function () {
+  //   $('.group').hide();
+  //   $('#'+$(this).val()).show();
+  });
+// });
+
+
+// $(document).ready(function () {
+//   $('.group').hide();
+//   $('#option1').show();
+//   $('#selectMe').change(function () {
+//     $('.group').hide();
+//     $('#'+$(this).val()).show();
+//   })
+// });
