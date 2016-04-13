9925663853273e3e524b3c138ca151a2a10709ad
Amy Wall <amymariewall@gmail.com>
2016-04-13 15:40:40 -0500

    About to start tearing this up to change direction.

--- app/assets/javascripts/tutorials.js
+++ app/assets/javascripts/tutorials.js
@@ -7,38 +7,41 @@ $("#showNewChapterForm").on('click', function(event){
       $("#showNewChapterForm").hide();
     });
 
-$(document).ready(function () {
-  $('.chapterContentDisplay').hide();
-  $('#chapterTitlesAndDescriptions').show();
-  $('.showChapterTab').on('click', function (event) {
-    var chapterTab = event.target;
-    var chapterId = chapterTab.parentElement.value;
-    // var chapterDisplay = document.getElementById("chapter"+chapterId)
-    // var chapterDisplayDiv = document.getElementById(chapterId);
-    $('#chapterTitlesAndDescriptions').hide();
-    $('.chapterContentDisplay').hide();
-    $('#chapterContentDisplay'+chapterId).show();
-    // chapterDisplay.style.display = "";
-    // $('#chapter'+$(chapterId).val()).show();
-  // $('.showChapterTab').on('click', function(event) {
-  //   var tab = event.target;
-  //   var chapterNumber = tab.value;
-  //   $('#'+chapterNumber).show();
-  })
 
-  // $('#option1').show();
-  // $('#selectMe').change(function () {
-  //   $('.group').hide();
-  //   $('#'+$(this).val()).show();
-  });
-// });
+// $(document).ready(function() {
+//   $('#tutorial').show();
+//   // $('#chapterTitlesAndDescriptions').show();
+//   $('#chapterDisplay').hide();
 
-
-// $(document).ready(function () {
-//   $('.group').hide();
-//   $('#option1').show();
-//   $('#selectMe').change(function () {
-//     $('.group').hide();
-//     $('#'+$(this).val()).show();
+//   $('.showChapterTab').on('click', function (event) {
+//     var chapterTab = event.target;
+//     var chapterId = chapterTab.parentElement.value;
+//     // var chapterDisplay = document.getElementById("chapter"+chapterId)
+//     // var chapterDisplayDiv = document.getElementById(chapterId);
+//     $('#chapterTitlesAndDescriptions').hide();
+//     $('.chapterContentDisplay').hide();
+//     $('#chapterContentDisplay'+chapterId).show();
+//     // chapterDisplay.style.display = "";
+//     // $('#chapter'+$(chapterId).val()).show();
+//   // $('.showChapterTab').on('click', function(event) {
+//   //   var tab = event.target;
+//   //   var chapterNumber = tab.value;
+//   //   $('#'+chapterNumber).show();
 //   })
-// });
+
+//   // $('#option1').show();
+//   // $('#selectMe').change(function () {
+//   //   $('.group').hide();
+//   //   $('#'+$(this).val()).show();
+//   });
+// // });
+
+
+// // $(document).ready(function () {
+// //   $('.group').hide();
+// //   $('#option1').show();
+// //   $('#selectMe').change(function () {
+// //     $('.group').hide();
+// //     $('#'+$(this).val()).show();
+// //   })
+// // });
