dbb2321be7a93042a5e39dd634ea3fae5ed319b3
Thorin Butler <thorin.butler@gmail.com>
2016-04-14 14:58:34 -0500

    got the page working so that when you click on the appropriate button, the search results that are so tagged display and no others

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,6 @@
 class SearchesController < ApplicationController
   def index
+    @categories = %w(CATEGORY1 CATEGORY2 CATEGORY3 CATEGORY4 CATEGORY5)
     @tutorials = Tutorial.where(public: true, draft: false)
     @results = @tutorials.primary_search(params[:q])
   end
