132fff470a79b442afb48c0d871c0d75860a0fb0
Bradley Biehl <bradley.biehl@gmail.com>
2016-05-01 13:25:29 -0500

    starting with step.rb

--- app/models/step.rb
+++ app/models/step.rb
@@ -6,7 +6,9 @@ class Step < ActiveRecord::Base
   }
   validates_presence_of :title, :chapter_id, :start_time, :end_time
 
-  # Internal: 
+  # Defines start time and end time
+  #
+  # Returns a String
   def video_time_conversion_for_steps
     start_time = Time.at(self.start_time).utc.strftime("%H:%M:%S")
     end_time = Time.at(self.end_time).utc.strftime("%H:%M:%S")
@@ -14,6 +16,7 @@ class Step < ActiveRecord::Base
     return "#{start_time} - #{end_time}"
   end
 
+  #
   def get_draft_title_for_step
     return self.draft == true ? "DRAFT: #{self.title.upcase}" : "EDIT: #{self.title.upcase}"
   end
